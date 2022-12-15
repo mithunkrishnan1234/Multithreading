2c303418d5365378aa97a3dd93b3b158a0c36cbb
Unni Krishnan KP, Mithun Krishnan U <MithunKrishnanU_UnniKrishnanKP@comcast.com>
2022-12-15 13:59:34 +0530

    Adding new commit that shows another possibility of Multithreading

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         for(int i=0;i<=3;i++) {
             MultithreadThing mything = new MultithreadThing(i);
-            Thread mythread = new Thread(mything);
+            Thread mythread = new Thread(mything); //for thread this not needed
             mythread.start();
         }
        //mythread.join() for waiting until one thread is executed uses try catch
